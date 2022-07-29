e3b442d1446504c4b3055ab0d59557cb5f2878cc
yussan <main.yussan@gmail.com>
2022-07-29 19:36:56 +0700

    feat(feed & sitemap): added feed & sitemap, news & competition

---
+++ src/pages/api/feed/competition/index.js
@@ -0,0 +1,9 @@
+import { fetchFeedCompetition } from "../../../../services/feed";
+
+const FeedCompetitionApi = async (req, res) => {
+  const Response = await fetchFeedCompetition();
+  res.setHeader("Content-Type", "text/xml");
+  return res.status(200).send(Response);
+};
+
+export default FeedCompetitionApi;
